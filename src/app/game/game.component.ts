fffb805b37287a73088d134c91d6739c98b55277
Lukasz Barszczewski <lukasz.barszczewski@yahoo.com>
2024-10-30 13:25:56 +0100

    the pickCard function causes now picking the current card, deleting it out of the stack and pushing it into the playedCards array, also an animation of picking up the top card and putting it on the table has been created

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
 
+export class GameComponent implements OnInit {
+  game: Game | undefined;
+  pickCardAnimation = false;
+  currentCard: string = '';
+  
+  constructor() {
+    
+  }
+  
+  ngOnInit(): void {
+    this.newGame();
+  }
+  
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+    
+  }
+  
+  pickCard(event: Event) {
+    if (this.game && !this.pickCardAnimation) {  // Überprüft, ob 'game' nicht undefined ist
+      this.currentCard = this.game.stack.pop() || '';
+      this.pickCardAnimation = true;
+      
+
+
+
+      console.log(this.game.stack);
+      console.log('played_cards: ' + this.game.playedCards);
+      console.log(this.currentCard);
+    } else {
+      console.error('Game is not initialized.');
+      event.preventDefault();
+    }
+
+    setTimeout(() => {
+      this.game!.playedCards.push(this.currentCard);
+      this.pickCardAnimation = false;
+    }, 1000);
+  }
 }
---
+++ src/models/game.ts
@@ -0,0 +1,35 @@
+export class Game {
+    public players: string[] = [];
+    public stack: string[] = [];
+    public playedCards: string[] = [];
+    public currentPlayer: number = 0;
+
+    constructor() {
+        for (let index = 1; index < 14; index++) {
+            this.stack.push('spades_' + index);
+            this.stack.push('hearts_' + index);
+            this.stack.push('clubs_' + index);
+            this.stack.push('diamonds_' + index);   
+        }
+
+        shuffleCards(this.stack);
+    }
+}
+
+function shuffleCards(array: string[]): string[] {
+    let currentIndex = array.length,  randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+};
